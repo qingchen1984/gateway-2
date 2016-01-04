732c64a5ea03a0262ff04e8adc46f4d906f4a8c1
Rogerio Biondi <rogerio.biondi@gmail.com>
2016-01-04 15:20:04 -0200

    ERD model change
    Included two tables: Registration table for managing the
    auto-registration process and assignment of the device_group;
    Announcement table for monitoring the devices.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,28 +1,44 @@
+
+-- Create the IOTDB Schema
 CREATE SCHEMA IOTDB ;
 use IOTDB;
+
 -- Create table facts
 CREATE TABLE Facts (
-    fact VARCHAR(50),
-    year INT,
-    month INT,
-    day INT,
-    week_day INT,
-    hour INT,
-    minute INT,
-    second INT,
-    device_group INT,
-    device INT,
-    sensor INT,
-    data INT,
-    creationDate DATETIME
+    fact VARCHAR(50) NOT NULL,
+    year INT NOT NULL,
+    month INT NOT NULL,
+    day INT NOT NULL,
+    week_day INT NOT NULL,
+    hour INT NOT NULL,
+    minute INT NOT NULL,
+    second INT NOT NULL,
+    device_group VARCHAR(20) NOT NULL,
+    device VARCHAR(20) NOT NULL,
+    sensor INT NOT NULL,
+    data INT NOT NULL,
+    creationDate DATETIME NOT NULL
 );
 
 -- Create table Device Statistics
 CREATE TABLE DeviceStatistics (
-    device INT,
+    device VARCHAR(20),
     sensor INT,
     average FLOAT,
     deviation FLOAT,
     error FLOAT,
     creationDate DATETIME
 );
+
+-- Create table Announcement
+CREATE TABLE Announcement (
+  device VARCHAR(20) NOT NULL,
+  lastAnnouncementDate DATETIME
+)
+
+-- Create table Registration
+CREATE TABLE Registration (
+  device_group VARCHAR(20),
+  device VARCHAR(20),
+  registrationDate DATETIME
+)
