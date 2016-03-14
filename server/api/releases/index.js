69112b057aaa3db39d9208b4c985ba2743ae1e38
Diego Osse Fernandes <diego.osse@gmail.com>
2016-03-14 17:24:01 -0300

    OTA ESP8266 Update
    Now the gateway can handle ESP8266 OTA updates. The OTA updates respond
    over `/releases/ESP8266/update`.

--- server/api/releases/index.js
+++ server/api/releases/index.js
@@ -16,19 +16,17 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 */
+
 'use strict';
 
-var errors = require('./components/errors');
+var express = require('express');
+var controller = require('./releases.controller');
 
-module.exports = function(app) {
+var router = express.Router();
 
-  app.use('/api/gateway', require('./api/gateway'));
-  app.use('/releases', require('./api/releases'));
+// Routes for Root
+router.get('/:type', controller.show);
+router.get('/:type/update', controller.update);
 
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
 
-  // All other routes should redirect to the index.html
-  app.route('/*').get(errors[404]);
-};
+module.exports = router;
---
+++ server/model/release.model.js
@@ -0,0 +1,49 @@
+/*
+* Meccano IOT Model
+*
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var model = sequelize.define('Release', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    type: DataTypes.STRING(20),
+    major: DataTypes.INTEGER,
+    minor: DataTypes.INTEGER,
+    patch: DataTypes.INTEGER,
+    prerelease: DataTypes.STRING(20),
+    firmwareBlob: DataTypes.BLOB('medium')
+  }, {
+    getterMethods: {
+      version: function() {
+        var version = this.major + '.' + this.minor + '.' + this.patch;
+        if (this.prerelease)
+          version += '-' + this.prerelease;
+        return version;
+      }
+    }
+  });
+
+
+  return model;
+}
